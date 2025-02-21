ee13b90755f4f5454c7b08c84d836d990cc7c7bf
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-02-21 17:02:48 +0300

    commit

--- src/js/how-to-play.js
+++ src/js/how-to-play.js
@@ -1,29 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.querySelector('.play_heading div');
   const arrowIcon = toggleBtn.querySelector('#bottom');
+  const cardList = document.querySelector('.card_list');
   const cards = document.querySelectorAll('.card_item');
 
   let isOpen = false;
-  const cardHeight = 420;
 
-  gsap.set(cards, { height: 0, opacity: 0, overflow: 'hidden' });
-  gsap.set(cards[0], { height: 'auto', opacity: 1 });
-  gsap.set(arrowIcon, { rotate: 0 });
+  gsap.set(cardList, { maxHeight: cards[0].offsetHeight + 'px', opacity: 1 });
+  gsap.set(cards, { opacity: index => (index === 0 ? 1 : 0) });
 
   toggleBtn.addEventListener('click', () => {
     if (!isOpen) {
-      // window.scrollBy({ top: cardHeight * 1.5, behavior: 'smooth' });
+      gsap.to(cardList, {
+        maxHeight: cardList.scrollHeight,
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power2.out',
+      });
 
       gsap.to(cards, {
-        height: index => (index === 0 ? 'auto' : cardHeight),
         opacity: 1,
         duration: 0.5,
         ease: 'power2.out',
         stagger: 0.1,
       });
     } else {
+      gsap.to(cardList, {
+        maxHeight: cards[0].offsetHeight + 'px',
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power2.in',
+      });
+
       gsap.to(cards, {
-        height: index => (index === 0 ? 'auto' : 0),
         opacity: index => (index === 0 ? 1 : 0),
         duration: 0.5,
         ease: 'power2.in',
